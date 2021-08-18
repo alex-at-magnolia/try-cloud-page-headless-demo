9980d9e97cd51f43598305bc3088060c95e4a6c0
Alex Mansell <alex.mansell@magnlia-cms.com>
2021-08-18 09:28:20 +0100

    updates for content types sync

---
+++ build/precache-manifest.70b92060a24067466d036028586d4243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202f1d2353b2cf992f7797fcd7acf075",
+    "url": "/magnoliaAuthor/.resources/react-minimal-lm/webresources/build/index.html"
+  },
+  {
+    "revision": "f629c0fbaee0faf9f92e",
+    "url": "/magnoliaAuthor/.resources/react-minimal-lm/webresources/build/static/css/main.6f78cb8a.chunk.css"
+  },
+  {
+    "revision": "045b60119f8fde918ee5",
+    "url": "/magnoliaAuthor/.resources/react-minimal-lm/webresources/build/static/js/2.d22b103f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/magnoliaAuthor/.resources/react-minimal-lm/webresources/build/static/js/2.d22b103f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f629c0fbaee0faf9f92e",
+    "url": "/magnoliaAuthor/.resources/react-minimal-lm/webresources/build/static/js/main.9bc0831f.chunk.js"
+  },
+  {
+    "revision": "4451046a288dcc059147",
+    "url": "/magnoliaAuthor/.resources/react-minimal-lm/webresources/build/static/js/runtime-main.075150c6.js"
+  }
+]);
